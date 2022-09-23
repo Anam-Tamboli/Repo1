eae5370194a7cf25b3e74af2b2e4f6f1989e0e2a
Anam-Tamboli <tamboli.anam@Kpipartners.com>
2022-09-23 17:07:51 +0530

    committed

---
+++ Prelims/src/com/kpi/animals/Dog.java
@@ -0,0 +1,5 @@
+package com.kpi.animals;
+//public class Dog extends Animal
+public class Dog extends Omnivorous{
+
+}
--- Prelims/src/com/kpi/animals/Omnivorous.java
+++ Prelims/src/com/kpi/animals/Omnivorous.java
@@ -1,5 +1,5 @@
 package com.kpi.animals;
 
 public class Omnivorous {
-
+// i am omnivorous
 }
