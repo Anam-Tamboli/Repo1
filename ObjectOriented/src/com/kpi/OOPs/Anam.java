82cb421cea50af7823df11efea9fed317a331a33
Anam-Tamboli <tamboli.anam@Kpipartners.com>
2022-09-27 17:47:07 +0530

    commit for constructor and interface

---
+++ ObjectOriented/src/com/kpi/OOPs/Anam.java
@@ -0,0 +1,29 @@
+package com.kpi.OOPs;
+
+public class Anam implements Skeleton {
+
+	@Override
+	public void flesh() {
+		// TODO Auto-generated method stub
+		System.out.println();
+	}
+
+	@Override
+	public void brain() {
+		// TODO Auto-generated method stub
+		System.out.println();
+	}
+
+	@Override
+	public void bones() {
+		// TODO Auto-generated method stub
+		System.out.println();
+	}
+
+	@Override
+	public void iqType() {
+		// TODO Auto-generated method stub
+		System.out.println();
+	}
+
+}
---
+++ ObjectOriented/src/com/kpi/OOPs/Skeleton.java
@@ -0,0 +1,8 @@
+package com.kpi.OOPs;
+
+public interface Skeleton extends IQ {
+	void flesh();
+	void brain();
+	void bones();
+
+}
